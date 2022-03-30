4c36a0c55b4a1a98259798c030667a28f64f595d
iariss <ennimizi@gmail.com>
2022-03-30 15:35:04 +0000

    ajax

---
+++ AJAX/index.js
@@ -0,0 +1,22 @@
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+function addNewDoggo()
+{
+	const promise = fetch(DOG_URL);
+	const doggos = document.querySelector(".doggos");
+	promise
+		.then(function(response)
+		{
+			const procesingsPromise = response.json();
+			return (procesingsPromise);
+		})
+		.then(function(processedResponse){
+			const image = document.createElement("img");
+			image.src = processedResponse.message;
+			image.alt = "Cute Dog";
+			doggos.appendChild(image);
+		})
+}
+
+
+document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
